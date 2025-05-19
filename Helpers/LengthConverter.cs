f49abef83578253141f10f339d48d4b4e00760a5
Alexander Skvorchevskiy <43242733+notsecret32@users.noreply.github.com>
2025-05-19 03:58:09 +0300

    refactor: removed unnecessary methods from the LengthConverter class

--- Helpers/LengthConverter.cs
+++ Helpers/LengthConverter.cs
@@ -6,16 +6,6 @@
         private const double MetersToMillimetersFactor = 1000;
 
         /// <summary>
-        /// Преобразует метры в миллиметры.
-        /// </summary>
-        /// <param name="meters">Значение в метрах.</param>
-        /// <returns>Значение в миллиметрах.</returns>
-        public static double ConvertMetersToMillimeters(double meters)
-        {
-            return meters * MetersToMillimetersFactor;
-        }
-
-        /// <summary>
         /// Преобразует миллиметры в метры.
         /// </summary>
         /// <param name="millimeters">Значение в миллиметрах.</param>
